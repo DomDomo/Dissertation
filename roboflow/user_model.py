997fe92e9665971ad7f3a03120c8188922b10b12
DomDomo <g.domis02@gmail.com>
2024-03-26 04:56:12 +0200

    Standardize WebUI model output to work with Supervision

--- roboflow/user_model.py
+++ roboflow/user_model.py
@@ -5,6 +5,7 @@ import json
 import cv2
 import glob
 import os
+import copy
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -21,19 +22,24 @@ ROBOFLOW_PROJECT_VERSION = 3
 MODEL_NAME = "hauwei"
 
 
-def process_prediction_output(data):
+def get_prediction_output(model, image_path, overlap_threshold):
+    overlap_threshold *= 100
+
+    pred = model.predict(
+        image_path, confidence=0, overlap=overlap_threshold).json()
+    
     # Add the image_path to the image attribute
-    image_path = data['predictions'][0]['image_path']
-    data['image']['image_path'] = image_path
+    image_path = pred['predictions'][0]['image_path']
+    pred['image']['path'] = image_path
 
     # Extract the game folder and name from the image_path
     game_folder, image_name = os.path.split(image_path)
     game_name = os.path.basename(game_folder)
     base_name = os.path.splitext(image_name)[0]
 
-    data['title'] = f"{game_name}_{base_name}"
+    pred['title'] = f"{game_name}_{base_name}"
 
-    for prediction in data['predictions']:
+    for prediction in pred['predictions']:
         # Remove the detection_id, image_path, and prediction_type
         del prediction['detection_id']
         del prediction['image_path']
@@ -46,23 +52,18 @@ def process_prediction_output(data):
         # Add the center point to the prediction
         prediction['top_left'] = (top_left_x, top_left_y)
 
-    return data
+    return pred
 
 
-def detect_ui_elements(model, confidence_threshold, overlap_threshold, image_path):
-    # Standardize to percentage
-    confidence_threshold *= 100
-    overlap_threshold *= 100
+def display_pred_boxes(pred, confidence_threshold, image_path):
+    pred_copy = copy.deepcopy(pred)
 
-    pred = model.predict(
-        image_path, confidence=0, overlap=overlap_threshold).json()
+    pred_copy["predictions"] = \
+        [p for p in pred_copy["predictions"] if p["confidence"] >= confidence_threshold]
     
+    labels = [item["class"] for item in pred_copy["predictions"]]
 
-    labels = [item["class"] for item in pred["predictions"]]
-
-    processed_data = process_prediction_output(pred)
-
-    detections = sv.Detections.from_inference(pred)
+    detections = sv.Detections.from_inference(pred_copy)
     red = sv.Color(r=255, g=0, b=0)
 
     label_annotator = sv.LabelAnnotator(color=red)
@@ -75,7 +76,7 @@ def detect_ui_elements(model, confidence_threshold, overlap_threshold, image_pat
     annotated_image = label_annotator.annotate(
         scene=annotated_image, detections=detections, labels=labels)
 
-    return annotated_image, processed_data
+    return annotated_image
 
 
 if __name__ == "__main__":
@@ -104,21 +105,19 @@ if __name__ == "__main__":
         if not os.path.exists(prediction_folder):
             os.makedirs(prediction_folder)
 
-        # Make predictions (you've already implemented this)
-        altered_image, processed_data = detect_ui_elements(
-            model, confidence_threshold, overlap_threshold, image_path)
+        processed_predictions = get_prediction_output(model, image_path, overlap_threshold)
+        all_predictions.append(processed_predictions)
+
+        altered_image = display_pred_boxes(
+            processed_predictions, confidence_threshold, image_path)
         
-        box_num = len(processed_data["predictions"])
+        box_num = len(processed_predictions["predictions"])
 
         # Save the altered image with the new title and path
         new_title = f"{title.split('.')[0]}_{confidence_threshold:.2f}_{box_num}.jpg"
         new_image_path = os.path.join(prediction_folder, new_title)
         cv2.imwrite(new_image_path, altered_image)
-
-
-        # Save prediction data:
-        all_predictions.append(processed_data)
-
+        
         print(
             f"Image '{title}' from '{game}' predicted and saved in '{prediction_folder}'")
         
--- webui/webui.py
+++ webui/webui.py
@@ -1,9 +1,12 @@
 import os
 import glob
 import torch
-from PIL import Image, ImageDraw
+from PIL import Image
 from torchvision import transforms
+import supervision as sv
 import json
+import cv2
+import copy
 
 PATH_TO_MODEL = "./model/screenrecognition-web350k-vins.torchscript"
 PATH_TO_METADATA = "./metadata/class_map_vins_manual.json"
@@ -19,41 +22,79 @@ def load_model_and_metadata():
     return model, class_map
 
 
-def detect_ui_elements(model, class_map, image_path, confidence_threshold=0.4):
+def get_prediction_output(model, class_map, image_path):
     # Load and transform the input image
     img_transforms = transforms.ToTensor()
     rgb_image = Image.open(image_path).convert('RGB')
     img_input = img_transforms(rgb_image)
 
+
+    # Extract the game folder and name from the image_path
+    game_folder, image_name = os.path.split(image_path)
+    game_name = os.path.basename(game_folder)
+    base_name = os.path.splitext(image_name)[0]
+
     # Get predictions
     pred = model([img_input])[1]
 
-    # Draw bounding boxes and labels
-    draw = ImageDraw.Draw(rgb_image)
+    image_predictions = {
+        "predictions": [
+            {
+                "x": int((x1 + x2) // 2),
+                "y": int((y1 + y2) // 2),
+                "width": int(x2 - x1),
+                "height": int(y2 - y1),
+                "confidence": pred[0]['scores'][i].item(),
+                "class": class_map[str(int(pred[0]['labels'][i].item()))],
+                "class_id": int(pred[0]['labels'][i].item()),
+                "top_left": (int(x1), int(y1))
+            }
+            for i, (x1, y1, x2, y2) in enumerate([box.tolist() for box in pred[0]['boxes']])
+        ],
+        "image": {
+            "width": rgb_image.size[0],
+            "height": rgb_image.size[1],
+            "path": image_path
+        },
+        "title": f"{game_name}_{base_name}"
+    }
 
-    box_num = len(pred[0]['boxes'])
-    print(f"Boxes: {box_num}")
+    return image_predictions
 
-    for i in range(box_num):
-        conf_score = pred[0]['scores'][i]
-        if conf_score > confidence_threshold:
-            x1, y1, x2, y2 = pred[0]['boxes'][i]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            draw.rectangle([x1, y1, x2, y2], outline='red', width=4)
-            label = class_map[str(int(pred[0]['labels'][i]))]
-            draw.text((x1, y1), f"{label} {conf_score:.2f}", fill="red")
+def display_pred_boxes(pred, image_path, confidence_threshold):
+    pred_copy = copy.deepcopy(pred)
 
-    return rgb_image, box_num
+    pred_copy["predictions"] = \
+        [p for p in pred_copy["predictions"] if p["confidence"] >= confidence_threshold]
+    
+    labels = [item["class"] for item in pred_copy["predictions"]]
+
+    detections = sv.Detections.from_inference(pred_copy)
+    
+    label_annotator = sv.LabelAnnotator()
+    bounding_box_annotator = sv.BoundingBoxAnnotator()
+
+    image = cv2.imread(image_path)
+
+    annotated_image = bounding_box_annotator.annotate(
+        scene=image, detections=detections)
+    annotated_image = label_annotator.annotate(
+        scene=annotated_image, detections=detections, labels=labels)
+
+    return annotated_image
 
 
 if __name__ == "__main__":
     model, class_map = load_model_and_metadata()
+    model_name = "huawei_prediction"
 
     image_paths = glob.glob('../idle_images/**/*.jpg', recursive=False)
 
     confidence_threshold = 0.2
 
-    for image_path in image_paths:
+    all_predictions = []
+
+    for image_path in image_paths[:1]:
 
         # Extract game and title from the image path
         game, title = image_path.split('\\')[1:]
@@ -66,16 +107,23 @@ if __name__ == "__main__":
         if not os.path.exists(prediction_folder):
             os.makedirs(prediction_folder)
 
+        processed_predictions = get_prediction_output(model, class_map, image_path)
+        all_predictions.append(processed_predictions)
+
         # Make predictions (you've already implemented this)
-        altered_image, box_num = detect_ui_elements(
-            model, class_map, image_path, confidence_threshold)
+        altered_image = display_pred_boxes(processed_predictions, image_path, confidence_threshold)
+
+        box_num = len(processed_predictions["predictions"])
 
         # Save the altered image with the new title and path
         new_title = f"{title.split('.')[0]}_{confidence_threshold:.2f}_{box_num}.jpg"
         new_image_path = os.path.join(prediction_folder, new_title)
-        altered_image.save(new_image_path)
+        cv2.imwrite(new_image_path, altered_image)
 
         print(
             f"Image '{title}' from '{game}' predicted and saved in '{prediction_folder}'")
+        
+    with open(f"{model_name}s.json", 'w') as f:
+        json.dump(all_predictions, f)
 
     print("Done.")
