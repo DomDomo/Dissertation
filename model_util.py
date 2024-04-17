1560ee3b1db4c47194d86537a9946a6618a26688
DomDomo <g.domis02@gmail.com>
2024-04-17 22:34:11 +0300

    Add dead zones and fix annotation structure

--- model_util.py
+++ model_util.py
@@ -16,18 +16,28 @@ def draw_center_dots(image, pred):
     return image
 
 
-def make_prediction_image(current, confidence, folder):
-    with open(f"./{folder}/current_predictions.json", 'r') as f:
-        data = json.load(f)
+def draw_dead_zones(image, box_coords, fix):
+    overlay = image.copy()
+    # The opacity level of the overlay, between 0 (completely transparent) and 1 (completely opaque)
+    opacity = 0.5
 
-    pred = data[0]
-    image_path = data[0]["image"]["path"]
+    for box in box_coords:
+        top_left = (box[0], box[1] - fix)
+        bottom_right = (box[2], box[3] - fix)
+        cv2.rectangle(overlay, top_left, bottom_right,
+                      (0, 0, 255), -1)  # -1 fills the rectangle
 
-    pred["predictions"] = \
-        [p for p in pred["predictions"] if p["confidence"] >= confidence]
+    # Blend the original image with the overlay
+    cv2.addWeighted(overlay, opacity, image, 1 - opacity, 0, image)
 
-    labels = [str(round(p["confidence"], 5)) for p in pred["predictions"]]
-    detections = sv.Detections.from_inference(pred)
+    return image
+
+
+def make_prediction_image(folder, filename, current, data, dead_zones, crop_fix):
+    image_path = f"./{folder}/{filename}"
+
+    labels = [str(round(p["confidence"], 5)) for p in data["predictions"]]
+    detections = sv.Detections.from_inference(data)
 
     label_annotator = sv.LabelAnnotator()
     bounding_box_annotator = sv.BoundingBoxAnnotator()
@@ -41,27 +51,57 @@ def make_prediction_image(current, confidence, folder):
     annotated_image = label_annotator.annotate(
         scene=annotated_image, detections=detections, labels=labels)
 
-    annotated_image = draw_center_dots(annotated_image, pred)
+    annotated_image = draw_center_dots(annotated_image, data)
+    annotated_image = draw_dead_zones(annotated_image, dead_zones, crop_fix)
 
     cv2.imwrite(f'./{folder}/round_{current}.jpg', annotated_image)
 
 
-def get_wanted_clicks(filename, folder, crop_fix, confidence):
-
+def get_predictions(folder, filename):
     predictions = get_webui_predictions(f"./{folder}/{filename}")
-    predictions["predictions"] = \
-        [p for p in predictions["predictions"] if p["confidence"] >= confidence]
 
-    # Want to press top to bottom, right to left
-    predictions["predictions"] = sorted(
-        predictions["predictions"], key=lambda p: (p['y'], -p['x']))
+    return predictions
 
-    # random.shuffle(predictions["predictions"])
 
+def filter_by_confidence(data, confidence):
+    data["predictions"] = \
+        [p for p in data["predictions"] if p["confidence"] >= confidence]
+
+    return data
+
+
+def sort_by_click_order(data, sort_type):
+    if sort_type == "random":
+        # Press randomly
+        random.shuffle(data["predictions"])
+    elif sort_type == "top_to_bottom":
+        # Press top to bottom, right to left
+        data["predictions"] = sorted(
+            data["predictions"], key=lambda p: (p['y'], -p['x']))
+
+    return data
+
+
+def save_prediction_data(folder, data):
     with open(f"./{folder}/current_predictions.json", 'w') as f:
-        json.dump([predictions], f)
+        json.dump([data], f)
 
+
+def is_point_in_box(point, box):
+    # Check if a point is within a box
+    x, y = point
+    x1, y1, x2, y2 = box
+    return x1 <= x <= x2 and y1 <= y <= y2
+
+
+def get_wanted_clicks(data, crop_fix, dead_zones):
     clicks = [(box["x"], box["y"] + crop_fix)
-              for box in predictions["predictions"]]
+              for box in data["predictions"]]
 
-    return clicks
+    filtered_clicks = []
+    for click in clicks:
+        # Add click to filtered_clicks if it's not in any of the dead zones
+        if not any(is_point_in_box(click, box) for box in dead_zones):
+            filtered_clicks.append(click)
+
+    return filtered_clicks
--- run.py
+++ run.py
@@ -2,12 +2,17 @@ import os
 import time
 
 from adb_util import *
-from model_util import make_prediction_image, get_wanted_clicks
+from model_util import *
 
 
 FOLDER = "CookieClicker"
 FILENAME = "screenshot.jpg"
-CONFIDENCE = 0.45
+CROP = True
+CONFIDENCE = 0.35
+ROUNDS = 20
+SORT_TYPE = "random"
+DEAD_ZONES = [(450, 2200, 1080, 2335)]
+
 
 if __name__ == "__main__":
     start_adb_server()
@@ -18,22 +23,29 @@ if __name__ == "__main__":
     if not os.path.exists(FOLDER):
         os.makedirs(FOLDER)
 
-    for i in range(1, 21):
-        print(f"Round: {i}")
+    for round in range(1, ROUNDS+1):
+        print(f"Round: {round}")
 
         # Create screenshot
-        pixels_to_remove = create_screenshot(
-            device, name=FILENAME, folder=FOLDER, crop=True)
+        removed_pixels = create_screenshot(
+            device, name=FILENAME, folder=FOLDER, crop=CROP)
 
-        # Turn on Pointer Location
+        # Turn on Pointer Location (for visualization)
         toggle_pointer_location(device, True)
 
         # Find where to click
-        clicks = get_wanted_clicks(
-            FILENAME, FOLDER, pixels_to_remove, CONFIDENCE)
+        prediction_data = get_predictions(FOLDER, FILENAME)
+        filtered_data = filter_by_confidence(prediction_data, CONFIDENCE)
+        sorted_data = sort_by_click_order(filtered_data, SORT_TYPE)
 
         # Create the Highlight Image
-        make_prediction_image(i, CONFIDENCE, FOLDER)
+        make_prediction_image(FOLDER, FILENAME, round,
+                              sorted_data, DEAD_ZONES, removed_pixels)
+
+        # Save Predictions
+        save_prediction_data(FOLDER, sorted_data)
+
+        clicks = get_wanted_clicks(sorted_data, removed_pixels, DEAD_ZONES)
 
         # Make the Clicks
         for x, y in clicks:
