11fb1a10456ab9aad6fa6e34f5e16890a44aaeba
DomDomo <g.domis02@gmail.com>
2024-04-18 22:43:52 +0300

    Find the precision, recall and f1 scores for each true image

--- analysis.py
+++ analysis.py
@@ -1,9 +1,12 @@
 import json
 import cv2
+import os
 
-GAME = "CookieClicker"
-FOLDER = f"ground_truth/{GAME}"
-FILENAME = "main"
+GROUND_TRUTH_FOLDER = "ground_truth"
+PREDICTIONS_FOLDER = "predictions"
+PREDICTIONS_FILE = "webui_predictions.json"
+ZONES_FILE = "zones.json"
+EVALUATION_RESULTS_FILE = "evaluation_results.json"
 
 
 def is_center_inside(center, box, fix):
@@ -13,92 +16,123 @@ def is_center_inside(center, box, fix):
     return x1 <= x <= x2 and y1 <= y <= y2
 
 
-def draw_predictions(image, centers, truth_zones, fix):
-    matched_truth_zones = set()
-    tp = 0
-    fp = 0
+def draw_prediction(image, center, truth_zones, fix):
+    x, y, confidence = center
+    is_tp = False
+    is_duplicate = False
 
-    for i, center in enumerate(centers):
-        x, y, confidence = center
+    for i, truth_zone in enumerate(truth_zones):
+        if is_center_inside((x, y), truth_zone, fix):
+            if i in image["matched_truth_zones"]:
+                is_duplicate = True
+            else:
+                is_tp = True
+                image["matched_truth_zones"].add(i)
+            break
 
-        # Check if the center of the predicted bounding box is inside any truth zone
-        is_tp = False
-        is_duplicate = False
-        for j, truth_zone in enumerate(truth_zones):
-            if is_center_inside((x, y), truth_zone, fix):
-                if j in matched_truth_zones:
-                    is_duplicate = True
-                else:
-                    is_tp = True
-                    matched_truth_zones.add(j)
-                break
+    color = (255, 0, 0) if is_tp else (
+        0, 165, 255) if is_duplicate else (0, 0, 255)
+    size = 10 if is_tp else 5
 
-        # Set the color based on the correctness and duplication
-        if is_tp:
-            color = (255, 0, 0)  # Blue for correct guess on a new truth zone
-            tp += 1
-        elif is_duplicate:
-            # Orange for correct guess on a duplicate truth zone
-            color = (0, 165, 255)
-            fp += 1
-        else:
-            color = (0, 0, 255)  # Red for incorrect guess
-            fp += 1
+    cv2.circle(image["data"], (x, y), size, color, -1)
+    cv2.putText(image["data"], f"Conf: {confidence:.2f}", (x + 10, y),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
 
-        # Draw the center point of the predicted bounding box
-        cv2.circle(image, (x, y), 5, color, -1)
-
-        # Label the center point with confidence score
-        label = f"Conf: {confidence:.2f}"
-        cv2.putText(image, label, (x + 10, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
-
-    fn = len(truth_zones) - len(matched_truth_zones)
-
-    return image, tp, fp, fn
+    if is_tp:
+        image["tp"] += 1
+    else:
+        image["fp"] += 1
 
 
 def calculate_metrics(tp, fp, fn):
     precision = tp / (tp + fp) if tp + fp > 0 else 0
     recall = tp / (tp + fn) if tp + fn > 0 else 0
-    f1_score = 2 * (precision * recall) / (precision +
-                                           recall) if precision + recall > 0 else 0
-
+    f1_score = 2 * precision * recall / \
+        (precision + recall) if precision + recall > 0 else 0
     return precision, recall, f1_score
 
 
+def process_image(image_data, game, truth_zones, crop_fix, predictions):
+    filename = image_data["title"]
+    true_image_path = os.path.join(
+        GROUND_TRUTH_FOLDER, game, f"true_{filename}.jpg")
+    true_image = cv2.imread(true_image_path)
+
+    image = {
+        "data": true_image.copy(),
+        "matched_truth_zones": set(),
+        "tp": 0,
+        "fp": 0
+    }
+
+    title = f"{game}_{filename}"
+    preds = next((obj for obj in predictions if obj["title"] == title), None)
+
+    if preds:
+        centers = [(pred["x"], pred["y"], pred["confidence"])
+                   for pred in preds["predictions"]]
+        centers.sort(key=lambda x: x[2], reverse=True)
+
+        for center in centers:
+            draw_prediction(image, center, truth_zones, crop_fix)
+
+    fn = len(truth_zones) - len(image["matched_truth_zones"])
+    precision, recall, f1_score = calculate_metrics(
+        image["tp"], image["fp"], fn)
+
+    output_path = os.path.join(PREDICTIONS_FOLDER, f"{
+                               game}_result_{filename}.jpg")
+    cv2.imwrite(output_path, image["data"])
+
+    return {
+        "filename": filename,
+        "true_positives": image["tp"],
+        "false_positives": image["fp"],
+        "false_negatives": fn,
+        "precision": precision,
+        "recall": recall,
+        "f1_score": f1_score
+    }
+
+
+def process_game(game, game_data, predictions):
+    crop_fix = game_data["crop_fix"]
+    game_results = []
+
+    for image_data in game_data["images"]:
+        truth_zones = image_data["zones"]
+        result = process_image(
+            image_data, game, truth_zones, crop_fix, predictions)
+        game_results.append(result)
+
+    return game_results
+
+
 if __name__ == "__main__":
-    true_image = cv2.imread(f"./{FOLDER}/true_{FILENAME}.jpg")
-
-    with open(f'./predictions/webui_predictions.json', 'r') as f:
-        data = json.load(f)
-
-    title = f"{GAME}_{FILENAME}"
-    preds = [obj for obj in data if obj["title"] == title][0]
-
-    centers = [(pred["x"], pred["y"], pred["confidence"])
-               for pred in preds["predictions"]]
-    centers.sort(key=lambda x: x[2], reverse=True)
-
-    with open(f'./ground_truth/zones.json', 'r') as f:
+    with open(os.path.join(GROUND_TRUTH_FOLDER, ZONES_FILE), 'r') as f:
         truth_data = json.load(f)
 
-    true_game_zones = truth_data[GAME]
-    truth_zones = [
-        image for image in true_game_zones["images"] if image["title"] == FILENAME][0]["zones"]
-    crop_fix = true_game_zones["crop_fix"]
+    with open(os.path.join(PREDICTIONS_FOLDER, PREDICTIONS_FILE), 'r') as f:
+        predictions = json.load(f)
 
-    result_image, tp, fp, fn = draw_predictions(
-        true_image.copy(), centers, truth_zones, crop_fix)
+    results = {}
 
-    cv2.imwrite(f'./predictions/result_{FILENAME}.jpg', result_image)
+    for game, game_data in truth_data.items():
+        game_results = process_game(game, game_data, predictions)
+        results[game] = game_results
 
-    precision, recall, f1_score = calculate_metrics(tp, fp, fn)
+    with open(os.path.join(PREDICTIONS_FOLDER, EVALUATION_RESULTS_FILE), 'w') as f:
+        json.dump(results, f, indent=4)
 
-    print("Evaluation Metrics:")
-    print(f"True Positives (TP): {tp}")
-    print(f"False Positives (FP): {fp}")
-    print(f"False Negatives (FN): {fn}")
-    print(f"Precision: {precision:.2f}")
-    print(f"Recall: {recall:.2f}")
-    print(f"F1 Score: {f1_score:.2f}")
+    for game, game_results in results.items():
+        print(f"Evaluation Metrics for {game}:")
+        for result in game_results:
+            print(f"  Filename: {result['filename']}")
+            print(f"  True Positives (TP): {result['true_positives']}")
+            print(f"  False Positives (FP): {result['false_positives']}")
+            print(f"  False Negatives (FN): {result['false_negatives']}")
+            print(f"  Precision: {result['precision']:.2f}")
+            print(f"  Recall: {result['recall']:.2f}")
+            print(f"  F1 Score: {result['f1_score']:.2f}")
+            print()
+        print()
